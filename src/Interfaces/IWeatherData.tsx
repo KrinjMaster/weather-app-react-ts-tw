1942978c5d6c2aeec73254d5c1563a0ac0178ef5
KrinjMaster <5opkasuka@gmail.com>
2023-02-20 23:01:43 +0300

    weather forecast added

---
+++ src/Interfaces/IWeatherData.tsx
@@ -0,0 +1,5 @@
+export interface IWeatherData {
+    time: string
+    temperature: number
+    windspeed: number
+}
--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,14 @@
 import { useState, ChangeEvent } from "react"
 import { IOption } from "./Interfaces/IOption"
+import { IWeatherData } from "./Interfaces/IWeatherData"
 
 const App = () => {
   const [term, setTerm] = useState<string>('')
   const [options, setOptions] = useState([])
+  const [showOptions, setShowOptions] = useState(true)
+  const [dataWeather, setDataWeather] = useState<IOption>(Object)
+  const [weather, setWeather] = useState<IWeatherData>(Object)
+  const [weatherState, setWeatherState] = useState(false)
   
   const getSearchOptions = (value: string) => {
     fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${value}&count=5`)
@@ -18,10 +23,21 @@ const App = () => {
     if (value === '') return
     
     getSearchOptions(value)
+    setShowOptions(true)
+    setWeatherState(false)
   }
 
   const onOptionSelect = (option: IOption) => {
     setTerm(option.name)
+    setShowOptions(false)
+    setDataWeather(option)
+  }
+
+  const onSubmit = (option: IOption) => {
+    fetch(`https://api.open-meteo.com/v1/forecast?latitude=${option.latitude}&longitude=${option.longitude}&current_weather=true`)
+    .then(response => response.json())
+    .then(data => setWeather(data.current_weather))
+    .then(data => setWeatherState(true))
   }
 
   return (
@@ -33,15 +49,20 @@ const App = () => {
         <div className="flex mt-10 md:mt-4 relative justify-center">
           <input type="text" value={term}  onChange={onInputChange} className="px-2 py-1 rounded-l-md border-2 border-white"/>
           <ul className="absolute top-9 bg-white rounded-b-md w-1/2">
-          {options && options.map((option: IOption) => ( 
+          {showOptions &&  options && options.map((option: IOption) => ( 
           <li key={option.id}>
             <button className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-4 cursor-pointer" onClick={() => onOptionSelect(option)}>{option.name}, {option.country}</button>
           </li>
           ))}
           </ul>
-          <button className="rounded-r-md border-2 border-zinc-100 hover;border-zinc-500 hover:text-zinc-100 px-2 py cursor-pointer">Search!</button>
+          <button onClick={() => onSubmit(dataWeather)} className="rounded-r-md border-2 border-zinc-100 hover;border-zinc-500 hover:text-zinc-100 px-2 py cursor-pointer">Search!</button>
+          {weatherState && <div className="absolute top-10 bg-white rounded h-32 w-48 align-middle">
+            <h1 className="font-normal">Current temperature: <br></br><span className="font-bold">{weather.temperature} °C</span></h1>
+            <h1>Current windspeed: <br></br>{weather.windspeed} km/h</h1>
+            <button onClick={() => setWeatherState(false)} className=" border rounded  hover:bg-zinc-700 hover:text-white px-4 cursor-pointer">Close</button>
+          </div>}
         </div>
-        <a href="https://open-meteo.com/" className="absolute bottom-1 font-bold text-white">&#9925; Weather data by Open-Meteo.com</a>
+        <a href="https://open-meteo.com/" className="absolute top-1 font-bold text-white">&#9925; Weather data by Open-Meteo.com</a>
       </section>
     </main>
   )
