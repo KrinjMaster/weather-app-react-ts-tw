a0e61582804f236906be988161a147ca53a9ecc9
KrinjMaster <5opkasuka@gmail.com>
2023-03-02 18:06:09 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-   plugins: [react()],
-   test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: '/src/setupTests.ts',
-   },
+  plugins: [react()],
+  base: '/weather-app-react-ts-tw/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: '/src/setupTests.ts',
+  },
 })
