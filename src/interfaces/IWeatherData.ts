34e352138b0d352538444dbb6b1f0012c9905b00
KrinjMaster <5opkasuka@gmail.com>
2023-03-04 00:17:43 +0300

    upd

--- src/interfaces/IWeatherData.ts
+++ src/interfaces/IWeatherData.ts
@@ -10,7 +10,7 @@ export interface IWeatherData {
   }
   hourly: {
     time: string[]
-    temperature_2m: []
-    relativehumidity_2m: []
+    temperature_2m: number[]
+    relativehumidity_2m: number[]
   }
 }
