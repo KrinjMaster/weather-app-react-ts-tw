49f446ff7add206e0974e307c00cca13787db476
KrinjMaster <5opkasuka@gmail.com>
2023-05-31 16:48:43 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -134,7 +134,7 @@ const App = () => {
 
   return (<>
     <div className="bg-transparent fixed top-0 text-white z-50">
-      <ul className="flex flex-row gap-5 z-50 w-screen justify-end px-5 text-3xl text-bold bg-white backdrop-blur-3xl bg-opacity-[0.5%]">
+      <ul className="flex flex-row gap-5 z-50 w-screen justify-end px-5 text-3xl text-bold bg-white backdrop-blur-3xl bg-opacity-[0.5%] font-extrabold">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#history">History</a></li>
@@ -142,43 +142,45 @@ const App = () => {
         <li><a href="#contacts">Contacts</a></li>
       </ul>
     </div>
-    <main className="flex flex-col items-center justify-center bg-[rgb(16,16,16)] h-fit w-full">
-      <section id="home" className="bg-[rgb(20,20,20)] backdrop-blur-lg w-full md:max-w-[900px] p-4 flex flex-col text-center md:px-10 lg:p-24 h-[105vh]">
-        <h1 className="text-4xl font-thin text-white">Krinj Master <span className="font-bold">Forecast</span></h1>
-        <p className="text-sm mt-2 text-white">Enter a city to get a weather forecast!</p>
-        <div className="flex mt-10 md:mt-4 relative justify-center">
-          <input type="text" value={term}  onChange={onInputChange} className="px-2 py-1 rounded-l-md border-2 border-white mt-[75px]"/>
-          <ul className="absolute top-9 bg-white rounded-xl w-[225px] mt-[80px] mr-[70px]">
-          {showOptions &&  options && options.map((option: IOption) => ( 
-          <li key={option.id}>
-            <button className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-4 cursor-pointer" onClick={() => onOptionSelect(option)}>{option.name}, {option.country}</button>
-          </li>
-          ))}
-          </ul>
-          <button onClick={() => onSubmit(weatherData)} className="rounded-r-md text-white border-2 border-zinc-100 hover;border-zinc-500 hover:text-zinc-100 px-2 py cursor-pointer mt-[75px]">Search!</button>
-          <div className="flex justify-center absolute top-0">
-            <h1 className="font-bold text-white mr-2">Metric Units</h1>
-            <div>
-              <input
-                className="mt-[0.3rem] mr-2 h-3.5 w-8 bg-red-500 appearance-none rounded-[0.4375rem] outline-none before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-white after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s]"
-                type="checkbox"
-                role="switch"
-                id="flexSwitchCheckDefault"
-                onChange={onUnitChange}
-                />
-              <label
-                className="inline-block hover:cursor-pointer text-white font-bold"
-                htmlFor="flexSwitchCheckDefault"
-                >Imperial Units</label>
+    <main className="flex flex-col items-center justify-center align-middle bg-[rgb(16,16,16)] h-fit w-full">
+      <section id="home" className="scale-100 backdrop-blur-lg w-full md:max-w-[900px] p-4 flex flex-col text-center md:px-10 lg:p-24 h-[105vh]">
+        <div className="scale-75 md:scale-100 lg:scale-150">
+          <h1 className="text-4xl font-thin text-white">Krinj Master <span className="font-bold">Forecast</span></h1>
+          <p className="text-sm text-white">Enter a city to get a weather forecast!</p>
+          <div className="flex relative justify-center">
+            <input type="text" value={term}  onChange={onInputChange} className="px-2 py-1 rounded-l-md border-2 border-white mt-[75px]"/>
+            <ul className="absolute top-9 bg-white rounded-xl w-[225px] mt-[80px] mr-[70px]">
+            {showOptions &&  options && options.map((option: IOption) => ( 
+            <li key={option.id}>
+              <button className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-4 cursor-pointer" onClick={() => onOptionSelect(option)}>{option.name}, {option.country}</button>
+            </li>
+            ))}
+            </ul>
+            <button onClick={() => onSubmit(weatherData)} className="rounded-r-md text-white border-2 border-zinc-100 hover;border-zinc-500 hover:text-zinc-100 px-2 py cursor-pointer mt-[75px]">Search!</button>
+            <div className="flex justify-center absolute top-0">
+              <h1 className="font-bold text-white mr-2">Metric Units</h1>
+              <div>
+                <input
+                  className="mt-[0.3rem] mr-2 h-3.5 w-8 bg-red-500 appearance-none rounded-[0.4375rem] outline-none before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-white after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s]"
+                  type="checkbox"
+                  role="switch"
+                  id="flexSwitchCheckDefault"
+                  onChange={onUnitChange}
+                  />
+                <label
+                  className="inline-block hover:cursor-pointer text-white font-bold"
+                  htmlFor="flexSwitchCheckDefault"
+                  >Imperial Units</label>
+              </div>
             </div>
-          </div>
+        </div>
             {DataState.loadData && 
-              <div className="absolute mt-[150px] bg-white bg-opacity-10 rounded-2xl h-[12rem] w-[15rem] align-middle ">
+              <div className="absolute left-0 right-0 ml-auto mr-auto mt-3 bg-white bg-opacity-10 rounded-2xl h-[12rem] w-[15rem] align-middle ">
                 <img src={LoadPng} width="100px" alt="loading" className="ml-[4rem] mt-5" />
                 <h1 className="text-[2rem]">Loading...</h1>
               </div>
             }
-            {DataState.dataState &&  <div className="absolute m-auto bg-white bg-opacity-10 rounded-2xl h-[18rem] w-[23rem] align-middle text-white xl:scale-125">
+            {DataState.dataState &&  <div className="absolute scale-75 2xl:scale-100 left-0 right-0 mt-0.5 ml-auto mr-auto bg-white bg-opacity-10 rounded-2xl h-[18rem] w-[23rem] align-middle text-white">
               <div className="flex flex-col mt-[45px]">
               <h1 className="font-bold content-center text-[50px]">
                 {weatherInfoObject?.current_weather.temperature} {weatherInfoObject?.hourly_units.temperature_2m}
@@ -225,7 +227,7 @@ const App = () => {
           <img src={BG} className="h-full w-full rounded-md opacity-20 absolute md:max-w-[900px]"/>
           <h2 className="font-thin text-3xl">Provider of weather forecasts, content and data based in Brookhaven, Georgia. The company offers up-to-date weather information and localized forecasts to people through television, online, mobile and tablet screens.</h2>
       </div>
-      <div id="history" className="h-screen bg-[rgb(20,20,20)] text-white font-bold text-center w-full md:max-w-[900px] flex flex-col px-5 justify-center items-center">
+      <div id="history" className="h-screen text-white font-bold text-center w-full md:max-w-[900px] flex flex-col px-5 justify-center items-center">
           <h1 className="text-8xl">History</h1>
           <h2 className="font-thin text-xl">Once upon a time, in a small town nestled in the foothills of a mountain range, there lived a young man named Alex. Alex had always been fascinated by the weather, and he spent countless hours studying meteorology and tracking weather patterns.
             One day, as he was walking home from his job at a local hardware store, Alex noticed that the sky was turning dark and ominous. He knew that a storm was brewing, but he had no way of knowing how severe it would be or when it would hit.
@@ -233,9 +235,9 @@ const App = () => {
             He spent months researching and developing his idea, pouring all of his time and energy into creating a weather forecasting system that would be both reliable and accessible. He worked tirelessly, often staying up late into the night, tweaking and refining his algorithms until they were as accurate as possible.
             Finally, after months of hard work, Alex launched his company. He called it "Krinj Forecast," and it quickly became a sensation. People all over the world were amazed by the accuracy of his forecasts, and soon, Krinj Forecast was the go-to source for weather information.</h2>
       </div>
-      <div id="crew" className="h-screen text-white font-bold text-center bg-[rgb(20,20,20)] w-full md:max-w-[900px]">
+      <div id="crew" className="h-screen text-white font-bold text-center w-full md:max-w-[900px]">
           <h1 className="text-8xl mt-5">Our workers:</h1>
-          <div className="flex justify-center gap-5 align-middle mt-[25vh]">
+          <div className="flex gap-3 align-middle justify-center h-full mt-[25vh]" >
             <div className="flex flex-col items-center">
               <img src={Worker1} className="w-32 rounded-lg"/>
               <h1 className="mt-1">Peter Parker</h1>
@@ -253,7 +255,7 @@ const App = () => {
             </div>
           </div>
       </div>
-      <div id="contacts" className="h-screen text-white font-bold text-center bg-[rgb(20,20,20)] w-full md:max-w-[900px] flex flex-col justify-center items-center">
+      <div id="contacts" className="h-screen text-white font-bold text-center w-full md:max-w-[900px] flex flex-col justify-center items-center">
         <div className="m-auto">
           <h1 className="text-8xl">Contacts:</h1>
           <div className="flex gap-5 justify-center">
