87d25c5b01c954863e3a1f0de454ed4aa6e096b2
KrinjMaster <5opkasuka@gmail.com>
2023-06-01 16:38:44 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,6 @@ import axios from "axios"
 import WindPng from '../public/assets/icons8-wind-100-2.png'
 import HumidityPng from '../public/assets/icons8-drop-of-blood-100.png'
 import LoadPng from '../public/assets/time-left.png'
-import Worker1 from '../public/assets/images.jpeg'
-import Worker2 from '../public/assets/500x500.jpg'
-import Worker3 from '../public/assets/rodney-amirebrahimi-bb10.jpg'
-import BG from '../public/assets/Brookhaven_Village.jpg'
 
 enum UnitActions {
   changeImperial = "changeImperial",
@@ -85,7 +81,7 @@ const App = () => {
     setWeatherData(null)
 
     if (value === '') return
-    
+
     getSearchOptions(value)
     setShowOptions(true)
     DataDispatch({type: DataActions.changeDataStateFalse})
@@ -133,21 +129,11 @@ const App = () => {
   }
 
   return (<>
-    <div className="bg-transparent fixed top-0 text-white z-50">
-      <ul className="flex flex-row gap-5 z-50 w-screen justify-end px-5 text-3xl text-bold bg-white backdrop-blur-3xl bg-opacity-[0.5%] font-extrabold">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#history">History</a></li>
-        <li><a href="#crew">Crew</a></li>
-        <li><a href="#contacts">Contacts</a></li>
-      </ul>
-    </div>
-    <main className="flex flex-col items-center justify-center align-middle bg-[rgb(16,16,16)] h-fit w-full">
-      <section id="home" className="scale-100 backdrop-blur-lg w-full md:max-w-[90vw] p-4 flex flex-col text-center md:px-10 lg:p-24 h-[105vh]">
-        <div className="scale-75 md:scale-100 lg:scale-150">
-          <h1 className="text-4xl font-thin text-white">Krinj Master <span className="font-bold">Forecast</span></h1>
-          <p className="text-sm text-white">Enter a city to get a weather forecast!</p>
-          <div className="flex relative justify-center">
+      <section id="home" className="h-screen w-screen p-4 flex flex-col text-center align-center md:px-10 lg:p-24">
+        <div className="">
+          <h1 className="text-4xl xl:text-6xl font-thin text-white">Krinj Master <span className="font-bold">Forecast</span></h1>
+          <p className="text-sm xl:text-lg text-white">Enter a city to get a weather forecast!</p>
+          <div className="flex relative justify-center xl:text-2xl">
             <input type="text" value={term}  onChange={onInputChange} className="px-2 py-1 rounded-l-md border-2 border-white mt-[75px]"/>
             <ul className="absolute top-9 bg-white rounded-xl w-[225px] mt-[80px] mr-[70px]">
             {showOptions &&  options && options.map((option: IOption) => ( 
@@ -180,8 +166,8 @@ const App = () => {
                 <h1 className="text-[2rem]">Loading...</h1>
               </div>
             }
-            {DataState.dataState &&  <div className="absolute scale-100 2xl:scale-150 2xl:mt-32 left-0 right-0 mt-0.5 ml-auto mr-auto bg-white bg-opacity-10 rounded-2xl h-[18rem] w-[23rem] align-middle text-white">
-              <div className="flex flex-col mt-[45px]">
+            {DataState.dataState &&  <div className="absolute left-0 right-0 mt-0.5 ml-auto mr-auto bg-white bg-opacity-10 rounded-2xl h-[18rem] xl:h-[24rem] w-[23rem] xl:text-3xl xl:w-[38rem] align-middle text-white">
+              <div className="flex flex-col mt-[45px] xl:gap-7">
               <h1 className="font-bold content-center text-[50px]">
                 {weatherInfoObject?.current_weather.temperature} {weatherInfoObject?.hourly_units.temperature_2m}
               </h1>
@@ -222,50 +208,6 @@ const App = () => {
           </div>}
         </div>
       </section>
-      <div id="about" className='h-screen bg-cover bg-opacity-30 md:max-w-[80vw] text-white font-bold text-center w-full flex flex-col justify-center items-center'>
-          <h1 className="text-8xl z-50 backdrop-blur-sm rounded-xl 2xl:backdrop-blur-md">About</h1>
-          <h2 className="text-3xl z-50 font-bold backdrop-blur-sm rounded 2xl:backdrop-blur-md">Provider of weather forecasts, content and data based in Brookhaven, Georgia. The company offers up-to-date weather information and localized forecasts to people through television, online, mobile and tablet screens.</h2>
-      </div>
-      <div id="history" className="h-screen text-white font-bold text-center w-full md:max-w-[90vw] flex flex-col px-5 justify-center items-center">
-          <h1 className="text-8xl">History</h1>
-          <h2 className="font-thin text-xl">Once upon a time, in a small town nestled in the foothills of a mountain range, there lived a young man named Alex. Alex had always been fascinated by the weather, and he spent countless hours studying meteorology and tracking weather patterns.
-            One day, as he was walking home from his job at a local hardware store, Alex noticed that the sky was turning dark and ominous. He knew that a storm was brewing, but he had no way of knowing how severe it would be or when it would hit.
-            That's when inspiration struck. What if he could create a company that would provide accurate and up-to-date weather forecasts to people all over the world? It was a bold idea, but Alex was determined to make it a reality.
-            He spent months researching and developing his idea, pouring all of his time and energy into creating a weather forecasting system that would be both reliable and accessible. He worked tirelessly, often staying up late into the night, tweaking and refining his algorithms until they were as accurate as possible.
-            Finally, after months of hard work, Alex launched his company. He called it "Krinj Forecast," and it quickly became a sensation. People all over the world were amazed by the accuracy of his forecasts, and soon, Krinj Forecast was the go-to source for weather information.</h2>
-      </div>
-      <div id="crew" className="h-screen text-white font-bold text-center w-full md:max-w-[90vw]">
-          <h1 className="text-8xl mt-5">Our workers:</h1>
-          <div className="flex gap-3 justify-center h-full mt-[25vh]" >
-            <div className="flex flex-col items-center">
-              <img src={Worker1} className="w-[135px] rounded-lg"/>
-              <h1 className="mt-1">Peter Parker</h1>
-              <h1 className="font-thin">Programmer</h1>
-            </div>
-            <div className="flex flex-col items-center">
-              <img src={Worker3} className="w-[145px] rounded-lg"/>
-              <h1 className="mt-1">James Charles</h1>
-              <h1 className="font-thin">Designer</h1>
-            </div>
-            <div className="flex flex-col items-center">
-              <img src={Worker2} className="w-[180px] rounded-lg"/>
-              <h1 className="mt-1">Alex White</h1>
-              <h1 className="font-thin">CEO</h1>
-            </div>
-          </div>
-      </div>
-      <div id="contacts" className="h-screen text-white font-bold text-center w-full md:max-w-[90vw] flex flex-col justify-center items-center">
-        <div className="m-auto">
-          <h1 className="text-8xl">Contacts:</h1>
-          <div className="flex gap-5 justify-center">
-            <a href="https://github.com/krinjmaster" target="_blank" className='text-blue-500 text-5xl'>GitHub</a>
-            <a href="https://github.com/krinjmaster" target="_blank" className='text-blue-500 text-5xl'>Telegram</a>
-            <a href="https://github.com/krinjmaster" target="_blank" className='text-blue-500 text-5xl'>OK</a>
-          </div>
-        </div>
-        <a href="https://open-meteo.com/" className="bottom-1 font-bold text-white opacity-50 mt-auto mr-auto">&#9925; Weather data by Open-Meteo.com</a>
-      </div>
-    </main>
   </>
   )
 }
